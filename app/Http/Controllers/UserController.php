da9458451282c5eca5b7142991ed8615c0de4b45
Nevi Marita Rimadhani <2131710025@student.polinema.ac.id>
2024-03-19 23:43:44 +0700

    Job3_EloquentORM_Prak6.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,13 @@ class UserController extends Controller
 {
     public function index()
     {
+        // tambah data user dengan Eloquent Model
         $data = [
             'nama' => 'Pelanggan Pertama',
         ];
         UserModel::where('username', 'customer-1')->update($data);
-
-        // coba akses model userModel
-        $user = UserModel::all(); // mengambil semua data dari tabel m_user
+        
+        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
