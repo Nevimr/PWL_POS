a8ce915eb206114a8a83c2e401d91a38abb70b81
Nevi Marita Rimadhani <2131710025@student.polinema.ac.id>
2024-05-15 01:39:50 +0700

    Job9_AUTHENTICATION,MIDDLEWARE_Prak

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ManagerController;
 use App\Models\User;
 
 Route::get('/', [WelcomeController::class, 'index']);
@@ -16,4 +19,20 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}/edit', [UserController::class, 'edit']);  // Menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']);     // Menyimpan perubahan data user
     Route::delete('/{id}', [UserController::class, 'destroy']); // Menghapus data user
+    Route::get('login', [AuthController::class, 'index'])->name('login');
+    Route::get('register', [AuthController::class, 'register'])->name('register');
+    Route::post('proses_login', [AuthController::class, 'proses_login'])->name('proses_login');
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('proses_register', [AuthController::class, 'proses_register'])->name('proses_register');
+
+    // kita atur juga untuk middleware menggunakan group pada routing
+    // didalamnya terdapat group untuk mengecek kondisi login
+    // jika user yang login merupakan admin maka akan diarahkan ke AdminController
+    // jika user yang login merupakan manager maka akan diarahkan ke UserController
+    Route::group(['middleware' => ['cek_login:1']], function () {
+        Route::resource('admin', AdminController::class);
+    });
+    Route::group(['middleware' => ['cek_login:2']], function () {
+        Route::resource('manager', ManagerController::class);
+    });
 });
